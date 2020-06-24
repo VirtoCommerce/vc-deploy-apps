5e8cc040d160501da623b2cd0aca2b58c9802f20
Leo <leon@entechsolutions.com>
2020-06-24 13:07:03 +0400

    VP-3331: Build image with modules on push

---
+++ webstore-app/overlays/dev/platform/install-modules.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import zipfile 
+import io 
+import urllib.request
+import json
+import zipfile
+import shutil
+
+def getZipData(url):
+    result = urllib.request.urlopen(url)
+    return result.read()
+
+modules = json.load('modules.json')
+
+for module in modules:
+    moduleId = module["Id"]
+    packageUrl = version["PackageUrl"]
+    destinationPath = moduleId
+    zipData = getZipData(packageUrl)
+    zipRef = zipfile.ZipFile(io.BytesIO(zipData))
+    zipRef.extractall(destinationPath)
+    print(moduleId, 'installed')
