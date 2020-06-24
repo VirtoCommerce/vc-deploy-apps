ed3f0b84fa7958f7b6764489cb93fab63e87f69d
Leo <leon@entechsolutions.com>
2020-06-24 13:23:03 +0400

    VP-3331: Fix file loading

--- webstore-app/overlays/dev/platform/install-modules.py
+++ webstore-app/overlays/dev/platform/install-modules.py
@@ -10,13 +10,13 @@ def getZipData(url):
     result = urllib.request.urlopen(url)
     return result.read()
 
-modules = json.load('./modules.json')
-
-for module in modules:
-    moduleId = module["Id"]
-    packageUrl = version["PackageUrl"]
-    destinationPath = moduleId
-    zipData = getZipData(packageUrl)
-    zipRef = zipfile.ZipFile(io.BytesIO(zipData))
-    zipRef.extractall(destinationPath)
-    print(moduleId, 'installed')
+with open('modules.json') as f:
+    modules = json.load(f)
+    for module in modules:
+        moduleId = module["Id"]
+        packageUrl = version["PackageUrl"]
+        destinationPath = moduleId
+        zipData = getZipData(packageUrl)
+        zipRef = zipfile.ZipFile(io.BytesIO(zipData))
+        zipRef.extractall(destinationPath)
+        print(moduleId, 'installed')
